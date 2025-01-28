653d02d78233ec9889e85d1a42686c0ec1405697
Vidhi Jindal <vidhijin11@gmail.com>
2025-01-28 18:36:42 +0530

    exception handling problems

---
+++ 03-02-2025/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            // Read two inputs
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            
+            // Try dividing a by b and print the result
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            // Handle division by zero
+            System.out.println("java.lang.ArithmeticException: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            // Handle invalid input
+            System.out.println("java.util.InputMismatchException");
+        }
+        scanner.close();
+    }
+}
